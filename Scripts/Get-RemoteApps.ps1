3b4f64a9ed751ba0fd73af377cd9479531f412ad
Elvis Abene <abene197@gmail.com>
2021-05-01 21:37:40 +0300

    updated test for remote computer

--- Scripts/Get-RemoteApps.ps1
+++ Scripts/Get-RemoteApps.ps1
@@ -1,1 +1,18 @@
-#
+$hive = [Microsoft.Win32.RegistryHive]::LocalMachine
+$hostname = [System.Net.Dns]::GetHostName()
+$regkey = "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\"
+$regconnection = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($hive, $hostname)
+$applist = $regconnection.OpenSubKey($regkey).GetSubkeyNames()
+foreach ($key in $applist) {
+    $app = $regconnection.OpenSubKey($regkey+$key)
+    $valuenames = $app.GetValueNames()
+    $dn = @{}
+    foreach($valuename in $valuenames) {
+        if ($valuename -eq "DisplayName") {
+            $table.Add($valuename, $app.GetValue($valuename))
+        }
+    }
+    $dn
+}
+#$soft.GetValueNames() | Select-Object @{Name = "Name"; Expression = {$_}},
+#@{Name = "Value"; Expression = {$soft.GetValue($_)}}
--- TestRemoteConnection/Program.cs
+++ TestRemoteConnection/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using Microsoft.Win32;
+using System.Collections;
 
 namespace TestRemoteConnection
 {
@@ -12,6 +13,11 @@ namespace TestRemoteConnection
     {
         static void Main(string[] args)
         {
+            const string MainKeyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\";
+            const string AppName = "DisplayName";
+            const string AppVersion = "DisplayVersion";
+
+
             // Получаем имя локального компьютера
             var hostname = Dns.GetHostName();
             Console.WriteLine($"Hostname: {hostname}");
@@ -20,17 +26,56 @@ namespace TestRemoteConnection
             var hive = RegistryHive.LocalMachine;
 
             // Подключаемся к этому разделу на удаленном(или локальном) компьютере
-
             //var regKey = RegistryKey.OpenBaseKey(hive, RegistryView.Registry32); // --local 
             var regKey = RegistryKey.OpenRemoteBaseKey(hive, hostname); // --remote тут вместо hostname нужно ввести имя удаленного компьютера
 
-            var subkeys = regKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\").GetSubKeyNames();
-
-            foreach (var subkey in subkeys)
+            // Получаем имена всех подразделов раздела SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ (каждый подраздел --- приложение)
+            var subkeys = regKey.OpenSubKey(MainKeyPath).GetSubKeyNames();
+            Hashtable[] apps = new Hashtable[subkeys.Length];
+            for (int i = 0; i < subkeys.Length; i++)
             {
-                Console.WriteLine(subkey);
+                apps[i] = new Hashtable();
+                var appKey = regKey.OpenSubKey(MainKeyPath + subkeys[i]);
+                foreach (var valueName in appKey.GetValueNames())
+                {
+                    if (valueName == AppName)
+                    {
+                        apps[i].Add(AppName, appKey.GetValue(AppName));
+                        //Console.WriteLine(valueName + " " + appKey.GetValue(valueName));
+                    }
+                    else if (valueName == AppVersion)
+                    {
+                        apps[i].Add(AppVersion, appKey.GetValue(AppVersion));
+                        //Console.WriteLine(valueName + " " + appKey.GetValue(valueName));
+                    }
+                }
             }
+            foreach (var table in apps)
+            ShowHashTable(table, " Table");
+
             Console.ReadKey();
+        }
+
+        private static void ShowHashTable(Hashtable hashTable, string title)
+        {
+            // Проверяем входные аргументы.
+            if (hashTable == null)
+            {
+                throw new ArgumentNullException(nameof(hashTable));
+            }
+
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+
+            // Выводим все имеющие пары хеш-значение
+            Console.WriteLine(title);
+            foreach (var key in hashTable.Keys)
+            {
+                Console.WriteLine($"\t{key} --- {hashTable[key]}");
+            }
+            Console.WriteLine();
         }
     }
 }
